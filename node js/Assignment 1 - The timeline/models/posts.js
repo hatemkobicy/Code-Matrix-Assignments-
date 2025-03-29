93b1a2b68a8fbf7c89545297f8603afe69fa5823
Hatem Kobicy <62618939+hatemkobicy@users.noreply.github.com>
2025-03-29 14:28:10 +0100

    Add files via upload

---
+++ node js/Assignment 1 - The timeline/models/posts.js
@@ -0,0 +1,25 @@
+// Data model for timeline posts
+const timelinePosts = [
+    {
+        name: "Michael Choi",
+        createdAt: "January 15th 2013",
+        message: "This is my message. This is my message. This is my message. This is my message. This is my message. This is my message. This is my message. This is my message. This is my message. This is my message. This is my message. This is my message. This is my message."
+    },
+    {
+        name: "Michael Choi",
+        createdAt: "January 23rd 2013",
+        message: "This is my message. This is my message. This is my message. This is my message. This is my message. This is my message. This is my message. This is my message. This is my message. This is my message. This is my message. This is my message. This is my message."
+    },
+    {
+        name: "Cory Whiteland",
+        createdAt: "January 15th 2013",
+        message: "This is my message. This is my message. This is my message. This is my message. This is my message. This is my message. This is my message. This is my message. This is my message. This is my message. This is my message. This is my message. This is my message."
+    },
+    {
+        name: "Cory Whiteland",
+        createdAt: "January 1st 2013",
+        message: "This is my message. This is my message. This is my message. This is my message. This is my message. This is my message. This is my message. This is my message. This is my message. This is my message. This is my message. This is my message. This is my message."
+    }
+];
+
+module.exports = timelinePosts;
