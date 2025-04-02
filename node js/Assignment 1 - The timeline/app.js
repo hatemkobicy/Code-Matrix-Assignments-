022a943b0f7ada7b53e54d07cd514e371f723414
Hatem Kobicy <62618939+hatemkobicy@users.noreply.github.com>
2025-04-02 18:31:02 +0200

    Update app.js

--- node js/Assignment 1 - The timeline/app.js
+++ node js/Assignment 1 - The timeline/app.js
@@ -3,22 +3,17 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-// Set EJS as the view engine
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-// Serve static files from the public directory
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Import data model for timeline posts
 const data = require('./models/posts');
 
-// Routes
 app.get('/', (req, res) => {
     res.render('index', { title: 'Timeline', posts: data });
 });
 
-// Start the server
 app.listen(port, () => {
     console.log(`Timeline app listening at http://localhost:${port}`);
 });
